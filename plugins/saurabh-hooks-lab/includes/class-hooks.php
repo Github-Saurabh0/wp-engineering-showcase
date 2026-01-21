54799daa06df65e829f7719e4d1e512cfca89155
Saurabh kushwaha <162017120+Github-Saurabh0@users.noreply.github.com>
2026-01-21 20:54:25 +0530

    Add manual custom action hook for admin-triggered execution

--- plugins/saurabh-hooks-lab/includes/class-hooks.php
+++ plugins/saurabh-hooks-lab/includes/class-hooks.php
@@ -7,10 +7,16 @@ if (!defined('ABSPATH')) exit;
 class Hooks {
 
     public static function register() {
-        add_action('wp_footer', [self::class, 'footer_message']);
+
+          add_action('wp_footer', [self::class, 'footer_message']);
+          add_action('saurabh_lab_manual_action', [self::class, 'manual_action']);
     }
 
     public static function footer_message() {
         echo '<p style="text-align:center;">Hooks loaded via OOP architecture</p>';
     }
+
+    public static function manual_action() {
+    error_log('Manual custom action triggered from admin panel');
+}
 }
