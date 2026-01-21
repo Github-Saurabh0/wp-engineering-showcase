86b4d6748ecc2028fc033842236de3cfa417d582
Saurabh kushwaha <162017120+Github-Saurabh0@users.noreply.github.com>
2026-01-21 20:54:55 +0530

    Add custom filter for admin-triggered filter demonstration

--- plugins/saurabh-hooks-lab/includes/class-filters.php
+++ plugins/saurabh-hooks-lab/includes/class-filters.php
@@ -7,8 +7,15 @@ if (!defined('ABSPATH')) exit;
 class Filters {
 
     public static function register() {
-        add_filter('the_content', [self::class, 'modify_content']);
-    }
+    add_filter('the_content', [self::class, 'modify_content']);
+
+    add_filter('saurabh_lab_manual_filter', [self::class, 'manual_filter']);
+}
+
+
+public static function manual_filter($text) {
+    return $text . ' → Modified using custom filter logic';
+}
 
     public static function modify_content($content) {
         return $content . '<p><strong>Content modified using OOP filter</strong></p>';
