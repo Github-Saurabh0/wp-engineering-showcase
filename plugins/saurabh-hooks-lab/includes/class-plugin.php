1a05c73dfffd499486ee071b6e70811304dcf1a2
Saurabh kushwaha <162017120+Github-Saurabh0@users.noreply.github.com>
2026-01-22 13:31:15 +0530

    Register shortcodes module in plugin bootstrap

--- plugins/saurabh-hooks-lab/includes/class-plugin.php
+++ plugins/saurabh-hooks-lab/includes/class-plugin.php
@@ -23,9 +23,11 @@ class Plugin {
     require_once plugin_dir_path(__DIR__) . 'includes/class-hooks.php';
     require_once plugin_dir_path(__DIR__) . 'includes/class-filters.php';
     require_once plugin_dir_path(__DIR__) . 'includes/class-admin.php';
+    require_once plugin_dir_path(__DIR__) . 'includes/class-shortcodes.php';
 
     Hooks::register();
     Filters::register();
     Admin::register();
+    Shortcodes::register();
 }
 }
